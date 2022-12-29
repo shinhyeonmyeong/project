ed784abbc19e914fe68c83b1f06f9cf45914076c
sinhyeonmyeong <sinhyeonmyeong@localhost>
2022-12-29 23:25:05 +0900

    init calender

---
+++ src/test/java/com/ezen/spring/web/CampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
